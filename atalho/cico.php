2ab72e4a32ca07b20bfb768b57765bae798a7d83
Celso <celsovasconcelos@msigroup.com.br>
2021-03-19 08:51:04 -0300

    hdaishda

--- atalho/cico.php
+++ atalho/cico.php
@@ -1,4 +1,284 @@
 <?php
+ 
+echo 'ldjalodjlakdjals';
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
+echo 'cicero';
+
+echo 'Leo';
+
+echo "Dogão";
+
+echo "Erik";
 
 echo 'cicero';
 
